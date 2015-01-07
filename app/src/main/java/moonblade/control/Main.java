12c426b7af26ef8f05c79f5745476680eec2645d
moonblade <moonblade168@gmail.com>
2015-01-07 17:28:59 +0530

    Basic Stuff works

--- app/src/main/java/moonblade/control/Main.java
+++ app/src/main/java/moonblade/control/Main.java
@@ -11,29 +11,70 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 
-public class Main extends Activity{
-    public TextView result;
-    Button update;
+public class Main extends Activity implements SensorEventListener{
+    public TextView result,result2;
+    Button stop,start;
+    int vxh=5,vxl=5,vyh=3,vyl=-3;
     private float vx,vy,vz;
     private TextView x,y,z;
     SensorManager mSensorManager;
+    ImageView dir;
     List<Sensor> mLight;
     Sensor acc;
-
+    private boolean flag=false;
     @Override
     public final void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        dir=(ImageView)findViewById(R.id.dir);
         setContentView(R.layout.activity_main);
         x=(TextView)findViewById(R.id.x);
         y=(TextView)findViewById(R.id.y);
         z=(TextView)findViewById(R.id.z);
         result=(TextView)findViewById(R.id.result);
+        result2=(TextView)findViewById(R.id.result2);
+        stop=(Button)findViewById(R.id.stop);
+        start=(Button)findViewById(R.id.start);
+        startsensor();
+//        dir.setImageResource(R.drawable.upright);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("stopped");
+                result2.setText("stopped");
+                onPause();
+            }
+        });
+        start.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //mSensorManager.registerListener(this,acc, SensorManager.SENSOR_DELAY_NORMAL);
+                if(mLight!=null)
+                {
+
+                    acc=mLight.get(0);
+                    String op=mLight.get(0).getName();
+                    if(acc==null)
+                        Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
+                    startsensor();
+//                    Toast.makeText(getApplicationContext(),"Doesnt work, start game again",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void startsensor() {
+        flag=false;
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mLight = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
         if(mLight!=null)
@@ -44,53 +85,90 @@ public class Main extends Activity{
             if(acc==null)
                 Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
 
-            mSensorManager.registerListener(mSensorListener,acc, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this,acc, SensorManager.SENSOR_DELAY_NORMAL);
         }
-
-
-
 
     }
 
-    private final SensorEventListener mSensorListener = new SensorEventListener() {
+    //private final SensorEventListener mSensorListener = new SensorEventListener() {
+    //};
 
-
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-
-
-
-        @Override
-        public final void onSensorChanged(SensorEvent event) {
-            // The light sensor returns a single value.
-            // Many sensors return 3 values, one for each axis.
-            //Toast.makeText(getApplicationContext(), "in", Toast.LENGTH_LONG).show();
-
-            vx=event.values[0];
-            vy=event.values[1];
-            vz=event.values[2];
-
-            x.setText("Moved in X : "+vx);
-            y.setText("Moved in Y : "+vy);
-            z.setText("Moved in Z : "+vz);
-
-            // Do something with this sensor value.
-
-
-
-
-
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(!flag) {
+            flag=true;
+            mSensorManager.unregisterListener(this);
+            mSensorManager = null;
         }
-
-
-
-    };
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(mSensorListener,acc, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this,acc, SensorManager.SENSOR_DELAY_NORMAL);
+        flag=false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        //Toast.makeText(getApplicationContext(), "in", Toast.LENGTH_LONG).show();
+
+        vx=event.values[0];
+        vy=event.values[1];
+        vz=event.values[2];
+
+        x.setText("Moved in X : "+vx);
+        y.setText("Moved in Y : "+vy);
+        z.setText("Moved in Z : "+vz);
+
+        if(vx>vxh)
+            result.setText("forward");
+        if(vx<vxl)
+            result.setText("reverse");
+
+        if(vy>vyh)
+            result2.setText("right");
+        else if(vy<vyl)
+            result2.setText("left");
+        else
+            result2.setText("no direction");
+/*        if(vx>vxh)
+        {
+            if(vy>vyh)
+                dir.setImageResource(R.drawable.upright);
+            else if(vy<vyl)
+                dir.setImageResource(R.drawable.upleft);
+            else
+                dir.setImageResource(R.drawable.up);
+
+        }
+        else if(vx<vxl)
+        {
+            if(vy>vyh)
+                dir.setImageResource(R.drawable.downright);
+            else if(vy<vyl)
+                dir.setImageResource(R.drawable.downleft);
+            else
+                dir.setImageResource(R.drawable.down);
+        }
+        else
+        {
+            if(vy>vyh)
+                dir.setImageResource(R.drawable.right);
+            else if(vy<vyl)
+                dir.setImageResource(R.drawable.left);
+            else
+                dir.setImageResource(R.drawable.stopped);
+
+        }*/
+
     }
 }
