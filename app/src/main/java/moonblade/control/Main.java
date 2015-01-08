9a81c198e787933b6032d0505c959955eee45e31
moonblade <moonblade168@gmail.com>
2015-01-08 21:58:12 +0530

    v2, added buttons like on hill climb rider

--- app/src/main/java/moonblade/control/Main.java
+++ app/src/main/java/moonblade/control/Main.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,10 +22,11 @@ import java.util.List;
 
 public class Main extends Activity implements SensorEventListener{
     public TextView result,result2;
-    Button stop,start;
+    ImageButton brake,gas;
     int vxh=5,vxl=-1,vyh=3,vyl=-3;
     private float vx,vy,vz;
     private TextView x,y,z;
+    boolean forward,reverse;
     SensorManager mSensorManager;
     ImageView dir;
     List<Sensor> mLight;
@@ -34,8 +36,13 @@ public class Main extends Activity implements SensorEventListener{
     public final void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_land);
+        brake=(ImageButton)findViewById(R.id.brake);
+        gas=(ImageButton)findViewById(R.id.gas);
         dir=(ImageView)findViewById(R.id.dir);
+/*
+
+
         x=(TextView)findViewById(R.id.x);
         y=(TextView)findViewById(R.id.y);
         z=(TextView)findViewById(R.id.z);
@@ -43,6 +50,8 @@ public class Main extends Activity implements SensorEventListener{
         result2=(TextView)findViewById(R.id.result2);
         stop=(Button)findViewById(R.id.stop);
         start=(Button)findViewById(R.id.start);
+*/
+
         startsensor();
 /*        stop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,56 +63,44 @@ public class Main extends Activity implements SensorEventListener{
                 onPause();
             }
         });*/
-        stop.setOnTouchListener(new View.OnTouchListener() {
+        brake.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-   /*                result.setText("stopped");
-                result2.setText("stopped");
-                */
-                        dir.setImageResource(R.drawable.stopped);
-                        onPause();
+                        brake.setImageResource(R.drawable.brakepressed);
+                        reverse=true;
                         break;
                     case MotionEvent.ACTION_UP:
-                        if(mLight!=null)
-                        {
-
-                            acc=mLight.get(0);
-                            String op=mLight.get(0).getName();
-                            if(acc==null)
-                                Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
-                            startsensor();
-                        }
+                        brake.setImageResource(R.drawable.brake);
+                        reverse=false;
                         break;
                     case MotionEvent.ACTION_CANCEL:
-                        if(mLight!=null)
-                        {
-
-                            acc=mLight.get(0);
-                            String op=mLight.get(0).getName();
-                            if(acc==null)
-                                Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
-                            startsensor();
-                        }
+                        brake.setImageResource(R.drawable.brake);
+                        reverse=false;
                         break;
                 }
                 return false;
             }
         });
-        start.setOnClickListener (new View.OnClickListener(){
+        gas.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                if(mLight!=null)
-                {
-
-                    acc=mLight.get(0);
-                    String op=mLight.get(0).getName();
-                    if(acc==null)
-                        Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
-                    startsensor();
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        gas.setImageResource(R.drawable.gaspressed);
+                        forward=true;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        gas.setImageResource(R.drawable.gas);
+                        forward=false;
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        gas.setImageResource(R.drawable.gas);
+                        forward=false;
+                        break;
                 }
-
+                return false;
             }
         });
 
@@ -162,35 +159,7 @@ public class Main extends Activity implements SensorEventListener{
         vy=event.values[1];
         vz=event.values[2];
 
-/*
-        x.setText("Moved in X : "+vx);
-        y.setText("Moved in Y : "+vy);
-        z.setText("Moved in Z : "+vz);
-*/
-
-        if(vx>vxh)
-            result.setText("forward");
-        if(vx<vxl)
-            result.setText("reverse");
-
-        if(vy>vyh)
-            result2.setText("right");
-        else if(vy<vyl)
-            result2.setText("left");
-        else
-            result2.setText("no direction");
-
-        if(vx>vxh)
-        {
-            if(vy>vyh)
-                dir.setImageResource(R.drawable.upright);
-            else if(vy<vyl)
-                dir.setImageResource(R.drawable.upleft);
-            else
-                dir.setImageResource(R.drawable.up);
-
-        }
-        else if(vx<vxl)
+        if(reverse)
         {
             if(vy>vyh)
                 dir.setImageResource(R.drawable.downright);
@@ -199,14 +168,23 @@ public class Main extends Activity implements SensorEventListener{
             else
                 dir.setImageResource(R.drawable.down);
         }
-        else {
-            if (vy > vyh)
+        else if(forward)
+        {
+            if(vy>vyh)
+                dir.setImageResource(R.drawable.upright);
+            else if(vy<vyl)
+                dir.setImageResource(R.drawable.upleft);
+            else
+                dir.setImageResource(R.drawable.up);
+        }
+        else
+        {
+/*            if (vy > vyh)
                 dir.setImageResource(R.drawable.right);
             else if (vy < vyl)
                 dir.setImageResource(R.drawable.left);
-            else
+            else*/
                 dir.setImageResource(R.drawable.stopped);
-
         }
 
     }
