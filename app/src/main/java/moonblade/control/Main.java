6bf0dacf1d27d1a140de14a4ccead730c395abc5
moonblade <moonblade168@gmail.com>
2015-01-08 18:50:53 +0530

    meh, need bluetooth now. made stop into ontouch

--- app/src/main/java/moonblade/control/Main.java
+++ app/src/main/java/moonblade/control/Main.java
@@ -9,6 +9,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,7 +22,7 @@ import java.util.List;
 public class Main extends Activity implements SensorEventListener{
     public TextView result,result2;
     Button stop,start;
-    int vxh=5,vxl=5,vyh=3,vyl=-3;
+    int vxh=5,vxl=-1,vyh=3,vyl=-3;
     private float vx,vy,vz;
     private TextView x,y,z;
     SensorManager mSensorManager;
@@ -33,8 +34,8 @@ public class Main extends Activity implements SensorEventListener{
     public final void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        dir=(ImageView)findViewById(R.id.dir);
         setContentView(R.layout.activity_main);
+        dir=(ImageView)findViewById(R.id.dir);
         x=(TextView)findViewById(R.id.x);
         y=(TextView)findViewById(R.id.y);
         z=(TextView)findViewById(R.id.z);
@@ -43,13 +44,51 @@ public class Main extends Activity implements SensorEventListener{
         stop=(Button)findViewById(R.id.stop);
         start=(Button)findViewById(R.id.start);
         startsensor();
-//        dir.setImageResource(R.drawable.upright);
-        stop.setOnClickListener(new View.OnClickListener() {
+/*        stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                result.setText("stopped");
+*//*                result.setText("stopped");
                 result2.setText("stopped");
+                *//*
+                dir.setImageResource(R.drawable.stopped);
                 onPause();
+            }
+        });*/
+        stop.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+   /*                result.setText("stopped");
+                result2.setText("stopped");
+                */
+                        dir.setImageResource(R.drawable.stopped);
+                        onPause();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if(mLight!=null)
+                        {
+
+                            acc=mLight.get(0);
+                            String op=mLight.get(0).getName();
+                            if(acc==null)
+                                Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
+                            startsensor();
+                        }
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        if(mLight!=null)
+                        {
+
+                            acc=mLight.get(0);
+                            String op=mLight.get(0).getName();
+                            if(acc==null)
+                                Toast.makeText(getApplicationContext(), "null",Toast.LENGTH_LONG).show();
+                            startsensor();
+                        }
+                        break;
+                }
+                return false;
             }
         });
         start.setOnClickListener (new View.OnClickListener(){
@@ -141,7 +180,7 @@ public class Main extends Activity implements SensorEventListener{
         else
             result2.setText("no direction");
 
-/*        if(vx>vxh)
+        if(vx>vxh)
         {
             if(vy>vyh)
                 dir.setImageResource(R.drawable.upright);
@@ -160,16 +199,15 @@ public class Main extends Activity implements SensorEventListener{
             else
                 dir.setImageResource(R.drawable.down);
         }
-        else
-        {
-            if(vy>vyh)
+        else {
+            if (vy > vyh)
                 dir.setImageResource(R.drawable.right);
-            else if(vy<vyl)
+            else if (vy < vyl)
                 dir.setImageResource(R.drawable.left);
             else
                 dir.setImageResource(R.drawable.stopped);
 
-        }*/
+        }
 
     }
 }
