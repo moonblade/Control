d64eb6245da1234d570a4135c3b38f1c056f6848
moonblade <moonblade168@gmail.com>
2015-01-11 19:42:33 +0530

    made a splash screen

--- app/src/main/java/moonblade/control/About.java
+++ app/src/main/java/moonblade/control/About.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.logging.Handler;
+
 import moonblade.control.R;
 
 public class About extends Activity {
---
+++ app/src/main/java/moonblade/control/MainSplashScreen.java
@@ -0,0 +1,57 @@
+package moonblade.control;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import moonblade.control.R;
+
+public class MainSplashScreen extends Activity {
+    private int delay=3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_splash_screen);
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(delay*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Main.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+}
